10ed01d160b25f622f8edced4448edab0bbddaf0
Elizabeth Perez <47160130+elizabethseis@users.noreply.github.com>
2019-04-11 16:39:10 -0500

    commit

--- ReadXMLFile/Program.cs
+++ ReadXMLFile/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace ReadXMLFile
 {
@@ -10,6 +11,31 @@ namespace ReadXMLFile
     {
         static void Main(string[] args)
         {
+            XmlTextReader reader = new XmlTextReader("C:\\Users\\elizabeth.perez\\source\\repos\\ReadXMLFile\\ReadXMLFile\\BreakfastMenuXML.xml");
+            while (reader.Read())
+            {
+                if (reader.IsStartElement())
+                {
+                   
+                    switch (reader.Name.ToString())
+                    {
+                        case "name":
+                            Console.WriteLine("Name: " + reader.ReadString());
+                            break;
+                        case "price":
+                            Console.WriteLine("Price: " + reader.ReadString());
+                            break;
+                        case "description":
+                            Console.WriteLine("Description: " + reader.ReadString());
+                            break;
+                        case "calories":
+                            Console.WriteLine("Calories: " + reader.ReadString());
+                            break;
+                    }
+                }
+                Console.WriteLine("");
+            }
+            Console.ReadLine();
         }
     }
 }
